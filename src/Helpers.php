4014248615ba12f1e435c53f45ba38591850b457
balsama <adam.balsam@acquia.com>
2023-08-02 10:35:50 -0400

    Use yaml for config

--- src/Helpers.php
+++ src/Helpers.php
@@ -1,20 +1,13 @@
 <?php
 
 namespace Balsama\Tempbot;
+use Symfony\Component\Yaml\Yaml;
 
 class Helpers
 {
-    public const BC_STATION_URL = 'api.weather.gov/stations/0258W/observations/latest';
-    public const LOGAN_STATION_URL = 'api.weather.gov/stations/KBOS/observations/latest';
-
     public static function getSensorIds(): array
     {
         return array_keys(self::sensorIpMap());
-    }
-
-    public static function getSensorIps(): array
-    {
-        return array_values(self::sensorIpMap());
     }
 
     public static function getSensorIpById(string $sensorId): string
@@ -23,32 +16,10 @@ class Helpers
         return $map[$sensorId];
     }
 
-    public static function getSensorIdByIp(string $sensorIp): string
-    {
-        $map = array_flip(self::sensorIpMap());
-        return $map[$sensorIp];
-    }
-
     public static function sensorIpMap(): array
     {
-        return [
-            'TB0101' => '192.168.7.161',
-            'TB0102' => '192.168.7.184',
-            'TB0201' => '192.168.7.160',
-            'TB0301' => '192.168.7.152',
-            'TB0302' => '192.168.7.156',
-            'TB0401' => '192.168.7.159',
-            'LOGAN' => self::LOGAN_STATION_URL,
-            'BC' => self::BC_STATION_URL,
-        ];
+        return Yaml::parseFile(__DIR__ . '/../config/sensors.yml');
     }
-
-    public static function availableSensor($host, $port = 80, $timeout = 3)
-    {
-        $fp = @fSockOpen($host, $port, $errno, $errstr, $timeout);
-        return $fp != false;
-    }
-
 
     /**
      * @return SensorReading[]
--- src/SensorReading.php
+++ src/SensorReading.php
@@ -8,6 +8,7 @@ use GuzzleHttp\Client;
 class SensorReading
 {
     private string $ip;
+    private string $id;
     private ?Response $response;
     public ?\stdClass $responseBody;
     private Client $client;
@@ -17,6 +18,7 @@ class SensorReading
     {
         $this->client = $client;
         $this->ip = Helpers::getSensorIpById($sensorId);
+        $this->id = $sensorId;
         $this->response = $this->getReading();
         $this->responseBody = ($this->response) ? json_decode($this->response->getBody()) : null;
         $this->responseType = $this->getType();
@@ -36,6 +38,7 @@ class SensorReading
         }
         return Helpers::c2f($this->responseBody->temperature);
     }
+
     public function getHumidity()
     {
         if (!$this->response) {
@@ -46,10 +49,11 @@ class SensorReading
         }
         return $this->responseBody->humidity;
     }
-    public function getTbId()
+
+    public function getTbId(): string
     {
         if (!$this->response) {
-            return null;
+            return $this->id;
         }
         if ($this->responseType === 'weather.gov') {
             $stationParts = explode('/', $this->responseBody->properties->station);
